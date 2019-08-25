c17b323bfd12a122b524092e18eafd7010ca7c5e
RawzaDigital <rawzadigital@gmail.com>
2019-08-25 17:30:21 +0700

    fix for initcontent in detail class

--- app/src/main/java/com/rdstudio/moovie/MovieDetailActivity.java
+++ app/src/main/java/com/rdstudio/moovie/MovieDetailActivity.java
@@ -2,13 +2,19 @@ package com.rdstudio.moovie;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.ViewCompat;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.jaeger.library.StatusBarUtil;
+
+import java.util.Objects;
 
 public class MovieDetailActivity extends AppCompatActivity {
 
@@ -18,11 +24,17 @@ public class MovieDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
-        StatusBarUtil.setTransparent(this);
-
+        initToolbar();
 
         //Movie movie = getIntent().getParcelableExtra(MOVIE_DETAIL);
     }
 
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
 }
