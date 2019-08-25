f9c83dd38764c2f7b60d91751af60b8f5eb47de6
RawzaDigital <rawzadigital@gmail.com>
2019-08-25 13:00:19 +0700

    redesign detail activity xml

--- app/src/main/java/com/rdstudio/moovie/MovieDetailActivity.java
+++ app/src/main/java/com/rdstudio/moovie/MovieDetailActivity.java
@@ -1,20 +1,26 @@
 package com.rdstudio.moovie;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.jaeger.library.StatusBarUtil;
+
 public class MovieDetailActivity extends AppCompatActivity {
 
-    public final String MOVIE_DETAIL = "movie_detail";
+   // public final String MOVIE_DETAIL = "movie_detail";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail);
-
-        Movie movie = getIntent().getParcelableExtra(MOVIE_DETAIL);
+        StatusBarUtil.setTransparent(this);
+        //Movie movie = getIntent().getParcelableExtra(MOVIE_DETAIL);
     }
+
+
 }
